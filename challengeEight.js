83cf980b2351800bb77f4b209b6f4901f89bc62d
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-01-30 03:29:23 +0100

    solve challenge 7 and 8

---
+++ challengeEight.js
@@ -0,0 +1,6 @@
+let username = "Rani"
+let message = "You have three new notifications"
+
+let messageToUser = message + ", " + username + "!  "
+
+console.log(messageToUser)
---
+++ challengeSeven.js
@@ -0,0 +1,5 @@
+let username = "Rani"
+let message = "You have three new notifications"
+
+
+console.log(message + ", " + username + "!")
