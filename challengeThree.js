1fc317a4505a7b35c4733f06c206f7acf1ae7f00
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-01-30 01:48:45 +0100

    complete challengeThree

---
+++ challengeThree.js
@@ -0,0 +1,8 @@
+let bonusPoints = 50;
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 25;
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints)
--- counterLesson.js
+++ counterLesson.js
@@ -1,6 +1,12 @@
 let firstBatch = 5;
 let secondBAtch = 7;
 
-let count = firstBatch + secondBAtch
+// let count = firstBatch + secondBAtch
+
+// count = "I am a King"
+const myPi = 3.43434
+let count = 5
+count = count + 4
+// myPi = "pi tester"
 
 console.log(count)
