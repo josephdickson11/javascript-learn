3ca14f206a0413a53f566d042fe31b155817ad61
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-01-30 03:13:32 +0100

    update counter with save button

--- counterLesson.js
+++ counterLesson.js
@@ -13,6 +13,11 @@ let count = 0;
 
 function increment(){
     count = count + 1
-    console.log(count)
+    document.getElementById("count-el").innerText = count
+    
 }
 
+function save() {
+    console.log(count)
+    
+}
