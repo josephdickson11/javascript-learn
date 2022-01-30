d3706db79b5e93959f5bb112827d46dfebd0e707
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-01-30 02:29:34 +0100

    getting started with functions

---
+++ functionsLesson.js
@@ -0,0 +1,17 @@
+// setting up the race
+
+
+function countdown(){
+    console.log(5);
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+ countdown()
+
+// GO!
+
+
+//start second race
+countdown()
