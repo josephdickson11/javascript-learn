0ee46b6eb59bb9c9c4ca899372d4880e1162c323
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-02-10 10:57:34 +0100

    complete challenge 13

---
+++ lessons/challenge13.js
@@ -0,0 +1,18 @@
+myPoints = 3;
+
+function add3Points() {
+    myPoints += 3;
+    
+}
+
+function remove1point() {
+    myPoints -= 1;
+}
+
+add3Points();
+add3Points();
+add3Points();
+remove1point();
+remove1point();
+
+console.log(myPoints)
