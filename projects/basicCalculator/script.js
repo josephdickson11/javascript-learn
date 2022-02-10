9aa6a9cf92b7542ff433bf591cb6c71738ad0dbf
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-02-10 13:57:29 +0100

    clean code

--- projects/basicCalculator/script.js
+++ projects/basicCalculator/script.js
@@ -5,60 +5,39 @@ document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
 function addBtn() {
-    console.log("btn clicked")
+    
     document.getElementById("sum-el").textContent = "sum: "
     let sumvalue2 = num1 + num2
-    console.log(sumvalue2)
     output = document.getElementById("sum-el").textContent
-    console.log(output)
     printValue = output + sumvalue2
-    console.log(printValue)
     document.getElementById("sum-el").textContent = printValue
-    output = "sum: "
     
 }
 
 function subBtn() {
-    console.log("btn clicked")
-    document.getElementById("sum-el").textContent = "sum: "
     
+    document.getElementById("sum-el").textContent = "sum: "
     let sumvalue2 = num1 - num2
-    console.log(sumvalue2)
     output = document.getElementById("sum-el").textContent
-    console.log(output)
     printValue = output + sumvalue2
-    console.log(printValue)
     document.getElementById("sum-el").textContent = printValue
-    output = "sum: "
     
 }
 
 function divBtn() {
-    console.log("btn clicked")
     document.getElementById("sum-el").textContent = "sum: "
-    
     let sumvalue2 = num1 / num2
-    console.log(sumvalue2)
     output = document.getElementById("sum-el").textContent
-    console.log(output)
     printValue = output + sumvalue2
-    console.log(printValue)
     document.getElementById("sum-el").textContent = printValue
-    output = "sum: "
     
 }
 
 function multBtn() {
-    console.log("btn clicked")
     document.getElementById("sum-el").textContent = "sum: "
-    
     let sumvalue2 = num1 * num2
-    console.log(sumvalue2)
     output = document.getElementById("sum-el").textContent
-    console.log(output)
     printValue = output + sumvalue2
-    console.log(printValue)
     document.getElementById("sum-el").textContent = printValue
-    output = "sum: "
     
 }
